2c1b5490e4173eb633ccc499b8740cd943f198fe
RODRIGODESS <diguinhodess@gmail.com>
2026-09-07 16:17:45 -0300

    chore: bump Android version to 2.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "br.com.monitordenoticias.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 284
-        versionName = "2.8.4"
+        versionCode = 290
+        versionName = "2.9.0"
     }
 
     signingConfigs {

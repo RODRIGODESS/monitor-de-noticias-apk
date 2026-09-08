d17b7afb6302c1680a13df8ab7483e395315da0d
RODRIGODESS <diguinhodess@gmail.com>
2026-09-08 00:43:28 -0300

    chore: subir versão Android para 3.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "br.com.monitordenoticias.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 305
-        versionName = "3.0.5"
+        versionCode = 306
+        versionName = "3.0.6"
     }
 
     signingConfigs {

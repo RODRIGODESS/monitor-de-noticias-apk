cb23f8d8658dddfddb69fb41b5465ac9c86c093c
RODRIGODESS <diguinhodess@gmail.com>
2026-09-09 10:08:54 -0300

    feat(windows): adicionar termos estabelecidos

---
+++ desktop/src/main/kotlin/br/com/monitordenoticias/android/DesktopEstablishedTerms.kt
@@ -0,0 +1,18 @@
+package br.com.monitordenoticias.android
+
+/** Termos estabelecidos exclusivos da edição Windows. */
+object DesktopEstablishedTerms {
+    val all: List<String> = listOf(
+        "CAPITANIA FLUVIAL",
+        "FRAGATA",
+        "SUBMARINO",
+        "COMANDANTE DA MARINHA",
+        "EXÉRCITO",
+        "FAB",
+        "MARINHA",
+        "MINISTRO DA DEFESA",
+        "MINISTÉRIO DA DEFESA",
+        "MAIOR NAVIO DA AMERICA LATINA",
+        "PROSUB"
+    )
+}
